144fb960dc11535fd7c85b71a687b93c19340726
Art Bajramaj <artbajramaj@gmail.com>
2024-09-12 18:23:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qr-code-app',
+  webDir: 'dist'
+};
+
+export default config;
